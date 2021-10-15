856ac313db519bf9f1ec008d3bb7a2b72a7df1aa
André Navarro <andre_farao123@hotmail.com>
2021-10-15 18:06:32 -0300

    Adding global styles and app fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,36 @@
 import React from 'react';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_600SemiBold,
+  Archivo_500Medium,
+} from '@expo-google-fonts/archivo';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
 import { Home } from './src/screens/Home';
+import theme from './src/styles/theme';
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_600SemiBold,
+    Archivo_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
